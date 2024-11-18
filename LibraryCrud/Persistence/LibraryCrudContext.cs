f9b8d9957e9d70f01cca6e2c02253c0391445282
Luiscvj <Luiscarlos.villanjurado@gmail.com>
2024-11-18 18:10:25 -0500

    ci: Configuring the project

--- LibraryCrud/Persistence/LibraryCrudContext.cs
+++ LibraryCrud/Persistence/LibraryCrudContext.cs
@@ -1,6 +1,24 @@
-﻿namespace LibraryCrud.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace LibraryCrud.Persistence
 {
-    public class LibraryCrudContext
+    public class LibraryCrudContext : DbContext
     {
+        public LibraryCrudContext(DbContextOptions options) : base(options)
+        { }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
+    
+
     }
+    
 }
